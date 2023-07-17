8a2b1773c20d0d6ae4827fb0ae8302b685f78e8c
qq382724935 <382724935@qq.com>
2023-07-17 14:16:15 +0800

    Update ReactNativeAmapSearch.h

--- ios/ReactNativeAmapSearch.h
+++ ios/ReactNativeAmapSearch.h
@@ -1,10 +1,18 @@
+/*
+ * @Author: 刘利军
+ * @Date: 2023-05-20 20:53:05
+ * @LastEditors: 刘利军
+ * @LastEditTime: 2023-07-17 14:16:02
+ * @Description: 
+ * @PageName: 
+ */
 
 #ifdef RCT_NEW_ARCH_ENABLED
 #import "RNReactNativeAmapSearchSpec.h"
 
 @interface ReactNativeAmapSearch : NSObject <NativeReactNativeAmapSearchSpec>
 #else
-#import <React/RCTBridgeModule.h>
+#import "RCTBridgeModule.h"
 #import <AMapSearchKit/AMapSearchKit.h>
 
 @interface ReactNativeAmapSearch : NSObject <RCTBridgeModule, AMapSearchDelegate>
