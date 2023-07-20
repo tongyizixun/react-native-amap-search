05e575d1dc625956e996bc4a42e3013ce015fed3
qq382724935 <382724935@qq.com>
2023-07-20 17:26:45 +0800

    1

--- ios/AmapSearch.h
+++
@@ -1,5 +0,0 @@
-#import "React/RCTBridgeModule.h"
-
-@interface AmapSearch : NSObject <RCTBridgeModule>
-
-@end
--- src/index.tsx
+++
@@ -1,9 +0,0 @@
-import { NativeModules } from 'react-native';
-
-type AmapSearchType = {
-  multiply(a: number, b: number): Promise<number>;
-};
-
-const { AmapSearch } = NativeModules;
-
-export default AmapSearch as AmapSearchType;
