731c47806877a9088da4a287076d16b25a25a447
qq382724935 <382724935@qq.com>
2023-07-21 17:26:23 +0800

    1

---
+++ src/index.tsx
@@ -0,0 +1,171 @@
+import { NativeModules, Platform } from 'react-native';
+import type {
+  AMapDistrictSearchParams,
+  AMapInputTipsSearchType,
+  AMapPOIAroundSearchType,
+  AMapPOIKeywordsSearchType,
+  AMapPOIPolygonSearchType,
+  AMapReGeocodeSearchParams,
+  AMapRegeoSearchParams,
+  AMapRoutePOISearchType,
+  ReactNativeAmapSearchType,
+} from './types';
+
+const LINKING_ERROR =
+  `The package 'react-native-amap-search' doesn't seem to be linked. Make sure: \n\n` +
+  Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
+  '- You rebuilt the app after installing the package\n' +
+  '- You are not using Expo Go\n';
+
+const AmapSearch: ReactNativeAmapSearchType = NativeModules.AmapSearch
+  ? NativeModules.AmapSearch
+  : new Proxy(
+      {},
+      {
+        get() {
+          throw new Error(LINKING_ERROR);
+        },
+      }
+    );
+
+// 初始化
+export const init = () => AmapSearch.initSDK();
+
+// poi搜索
+export const aMapPOIKeywordsSearch = async ({
+  keyword = '',
+  type = '',
+  city = '',
+  currentPage = 1,
+  pageSize = 10,
+  cityLimit = false,
+}: AMapPOIKeywordsSearchType) => {
+  return AmapSearch.aMapPOIKeywordsSearch(
+    keyword,
+    type,
+    city,
+    currentPage,
+    pageSize,
+    cityLimit
+  );
+};
+
+// 沿途道路搜索
+export const aMapRoutePOISearch = async ({
+  fromPoint,
+  toPoint,
+  mode = 0,
+  type = 0,
+  range = 250,
+}: AMapRoutePOISearchType) => {
+  return AmapSearch.aMapRoutePOISearch(fromPoint, toPoint, mode, type, range);
+};
+
+// 多边形搜索
+export const aMapPOIPolygonSearch = async ({
+  points,
+  keyword = '',
+  type = '',
+  city = '',
+  currentPage = 1,
+  pageSize = 10,
+}: AMapPOIPolygonSearchType) => {
+  return AmapSearch.aMapPOIPolygonSearch(
+    keyword,
+    type,
+    city,
+    points,
+    currentPage,
+    pageSize
+  );
+};
+
+// 周边搜索
+export const aMapPOIAroundSearch = async ({
+  latitude,
+  longitude,
+  keyword = '',
+  type = '',
+  city = '',
+  radius = 10000,
+  currentPage = 1,
+  pageSize = 10,
+}: AMapPOIAroundSearchType) => {
+  return AmapSearch.aMapPOIAroundSearch(
+    keyword,
+    type,
+    city,
+    latitude,
+    longitude,
+    currentPage,
+    pageSize,
+    radius
+  );
+};
+
+// 搜索提示
+export const aMapPOIInputTipsSearch = async ({
+  keyword = '',
+  city = '',
+  type = '',
+  latitude = 0,
+  longitude = 0,
+}: AMapInputTipsSearchType) => {
+  return await AmapSearch.aMapPOIInputTipsSearch(
+    keyword,
+    city,
+    type,
+    latitude,
+    longitude
+  );
+};
+
+// id搜索
+export const aMapPOIIDSearch = async (id: string) => {
+  return AmapSearch.aMapPOIIDSearch(id);
+};
+
+// 逆地理编码
+export const AMapReGeocodeSearch = async ({
+  latitude,
+  longitude,
+  radius = 1000,
+  latLonType = 'AMAP',
+  extensions = 'all',
+  poiType = '',
+  mode = 'distance',
+}: AMapReGeocodeSearchParams) => {
+  return await AmapSearch.AMapReGeocodeSearch(
+    latitude,
+    longitude,
+    radius,
+    latLonType,
+    extensions,
+    poiType,
+    mode
+  );
+};
+
+// 地理编码
+export const AMapGeocodeSearch = async ({
+  name,
+  city = '',
+  country = '',
+}: AMapRegeoSearchParams) => {
+  return AmapSearch.AMapGeocodeSearch(name, city, country);
+};
+
+// 城区获取
+export const AMapDistrictSearch = async ({
+  keyword = '',
+  pageSize = 20,
+  currentPage = 1,
+  subdistrict = 0,
+}: AMapDistrictSearchParams) => {
+  return await AmapSearch.aMapDistrictSearch(
+    keyword,
+    currentPage,
+    pageSize,
+    subdistrict
+  );
+};
---
+++ src/types/poi.d.ts
@@ -0,0 +1,157 @@
+/*
+ * @Author: 刘利军
+ * @Date: 2023-07-12 10:42:50
+ * @LastEditors: 刘利军
+ * @LastEditTime: 2023-07-12 14:46:26
+ * @Description:
+ * @PageName:
+ */
+
+export type AMapPageType = {
+  currentPage?: number;
+  pageSize?: number;
+};
+
+export type AMapLatLonType = {
+  latitude?: number;
+  longitude?: number;
+};
+
+export type AMapLatLonMustType = {
+  latitude: number;
+  longitude: number;
+};
+
+export type AMapPOICommonSearchType = {
+  keyword?: string;
+  type?: string;
+  city?: string;
+};
+
+export type AMapRoutePOISearchType = {
+  fromPoint: AMapLatLonType;
+  toPoint: AMapLatLonType;
+  mode: number;
+  type: number;
+  range: number;
+};
+
+// 关键字搜索
+export type AMapPOIKeywordsSearchType = AMapPOICommonSearchType &
+  AMapPageType & {
+    cityLimit?: boolean;
+  };
+
+// 搜索提示
+export type AMapInputTipsSearchType = AMapPOICommonSearchType & AMapLatLonType;
+
+// 多边形搜素
+export type AMapPOIPolygonSearchType = AMapPOICommonSearchType &
+  AMapPageType & {
+    points: AMapLatLonType[];
+  };
+
+//   周边搜索
+export type AMapPOIAroundSearchType = AMapPOICommonSearchType &
+  AMapLatLonMustType &
+  AMapPageType & { radius?: number; keyword?: string };
+
+// poi 搜索 list
+export type AMapPOISearchListItem = {
+  uid: string; // uid
+  name: string; // 名称
+  type: string; //兴趣点类型
+  typeCode: string; // 类型编码
+  latLonPoint: AMapLatLonMustType; // 经纬度
+  address: string; // 地址
+  province: string; // 省
+  provinceCode: string; // 省编码
+  city: string; // 市
+  cityCode: string; // 城市编码
+  district: string; // 区域名称
+  adCode: string; // 区域编码
+};
+
+// poi搜索回调
+export type AMapPOISearchResult = {
+  count: number;
+  list: AMapPOISearchListItem[];
+};
+
+// 逆地理编码
+export type AMapReGeocodeSearchLatLonType = 'AMAP' | 'GPS';
+export type AMapReGeocodeSearchExtensions = 'all' | 'base';
+export type AMapReGeocodeSearchMode = 'distance' | 'score';
+export type AMapReGeocodeSearchParams = AMapLatLonMustType & {
+  radius?: number;
+  latLonType?: AMapReGeocodeSearchLatLonType;
+  extensions?: AMapReGeocodeSearchExtensions;
+  mode?: AMapReGeocodeSearchMode;
+  poiType?: string;
+};
+
+export type AMapReGeocodeSearchResult = {
+  uid: string; // uid
+  name: string; // 名称
+  type: string; //兴趣点类型
+  typecode: number; // 类型编码
+  location: any;
+  address: string; // 地址
+  tel: number; // 电话
+  distance: any; //距中心点的距离，单位米
+  parkingType: any; //停车场类型，地上、地下
+  shopID: string; // 商铺id
+  postcode: number; //邮编
+  website: string; // 网址
+  email: string; //电子邮件
+  province: string; //省
+  pcode: number; //省编码
+  city: string; //市
+  citycode: number; //城市编码
+  district: string; //区域名称
+  adcode: number; // 区域编码
+};
+
+// 地理编码
+export type AMapRegeoSearchParams = {
+  name: string;
+  city?: string;
+  country?: string;
+};
+export type AMapGeocodeSearchResult = Array<{
+  postCode: string;
+  formatAddress: string;
+  latLonPoint: AMapLatLonMustType;
+  province: string;
+  district: string;
+  building: string;
+  neighborhood: string;
+  level: string;
+  city: string;
+  adcode: string;
+  country: string;
+}>;
+
+// 区域获取
+export type AMapDistrictSearchParams = {
+  keyword: string;
+  subdistrict?: number;
+} & AMapPageType;
+
+export type AMapDistrictSearchDistrictType = {
+  districts: AMapDistrictSearchDistrictsType;
+  polylines: string[];
+  name: string;
+  center: AMapLatLonMustType;
+  cityCode: string;
+  level: string;
+  adCode: string;
+};
+
+export type AMapDistrictSearchDistrictsType =
+  Array<AMapDistrictSearchDistrictType>;
+
+export type AMapDistrictSearchResult = {
+  count: number;
+  districts: AMapDistrictSearchDistrictsType;
+};
