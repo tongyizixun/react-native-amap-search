b2c41edf706e8b765a310108a5e6fc659ec9b957
qq382724935 <382724935@qq.com>
2023-07-25 13:42:11 +0800

    fix: 修复导出types报错

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,2 +1,10 @@
-export * from './poi';
-export * from './common';
+/*
+ * @Author: 刘利军
+ * @Date: 2023-07-21 16:32:11
+ * @LastEditors: 刘利军
+ * @LastEditTime: 2023-07-25 13:35:41
+ * @Description:
+ * @PageName:
+ */
+export * from './poi.d.ts';
+export * from './common.d.ts';
